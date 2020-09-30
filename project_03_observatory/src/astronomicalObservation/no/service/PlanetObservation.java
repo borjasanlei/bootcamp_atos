5ccd8da7225ae55ecd0ec6ac201baf337d59f1ee
borjasanlei <borja.sanchez.leirado@gmail.com>
2020-09-30 14:20:56 +0200

    Adding planetObservation class to project 03

---
+++ project_03_observatory/src/astronomicalObservation/no/service/PlanetObservation.java
@@ -0,0 +1,8 @@
+package astronomicalObservation.no.service;
+
+public class PlanetObservation {
+
+	public void investigateStar(Double coordinate1, Double coordinate2) {
+		System.out.println(String.format("Investigating a planet... looking at %f, %f coordinates", coordinate1, coordinate2));
+	}
+}
